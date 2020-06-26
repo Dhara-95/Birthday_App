5708747426346184df4969bbc757c776edd4db51
James Forster <jamesalexforster@gmail.com>
2020-06-26 11:32:00 +0100

    started implementing birthday and name params

--- app.rb
+++ app.rb
@@ -2,11 +2,19 @@ require 'sinatra/base'
 class Birthday < Sinatra::Base
 
   get '/' do
+    session[:name] = params[:name]
+    session[:birthday] = params[:birthday]
     erb :index
   end
 
   post '/birthday' do
-    
+    p "Name"
+    p session[:name]
+    p "Birthday"
+    p session[:birthday]
+    @name = session[:name]
+    @birthday = session[:birthday]
+    erb :birthday
   end
 
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,23 @@
 feature 'Visit homepage' do
-  scenario 'Homepage displays hello!' do
+  # scenario 'Homepage displays hello!' do
+  #   visit '/'
+  #   expect(page).to have_content "Hello!"
+  # end
+
+  scenario 'when submitting my name and birthday it will display' do
     visit '/'
-    expect(page).to have_content "Hello!"
+    fill_in :name, with: 'James'
+    fill_in :birthday, with: '1990/02/03'
+    click_button 'Submit'
+    expect(page).to have_content "Your birthday is 1990/02/03, James!"
   end
+
+  scenario 'when submitting Miris name and birthday it will display' do
+    visit '/'
+    fill_in :name, with: 'Miri'
+    fill_in :birthday, with: '1996/02/23'
+    click_button 'Submit'
+    expect(page).to have_content "Your birthday is 1996/02/23, Miri!"
+  end
+
 end
