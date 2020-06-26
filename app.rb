7318460259a75d9f96d7c5851822b78983dcafa6
Miri Michaelson <miriammichaelson@gmail.com>
2020-06-26 12:34:17 +0100

    Failing test, converting html date to ruby strfdate

--- app.rb
+++ app.rb
@@ -1,20 +1,32 @@
 require 'sinatra/base'
+require 'date'
+
+
 class Birthday < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
-    session[:name] = params[:name]
-    session[:birthday] = params[:birthday]
     erb :index
   end
 
   post '/birthday' do
-    p "Name"
-    p session[:name]
-    p "Birthday"
-    p session[:birthday]
+    session[:name] = params[:name]
+    session[:birthday] = params[:birthday]
     @name = session[:name]
     @birthday = session[:birthday]
-    erb :birthday
+    @current_date = Date.today.strftime('%d %b %Y')
+    #a string of date, laid out like - "25 June 2020"
+    p @current_date
+    #a string of date, laid out like - "1990/02/03"
+    p @birthday
+  #  p @birthday.strftime('%d %b %Y')
+    if @birthday == @current_date
+      erb :birthday
+    else
+      erb :birthday_countdown
+    end
+
   end
 
 end
